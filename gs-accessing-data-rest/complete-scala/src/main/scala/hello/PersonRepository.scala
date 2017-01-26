30627522fef5fbeef68d1d9feb88d91833b3cca5
Bernhard Wenzel <bernhard.wenzel@gmail.com>
2017-01-26 11:05:04 +0000

    fixed explicit import of java list

--- gs-accessing-data-rest/complete-scala/src/main/scala/hello/PersonRepository.scala
+++ gs-accessing-data-rest/complete-scala/src/main/scala/hello/PersonRepository.scala
@@ -1,5 +1,7 @@
 package hello
 
+import java.util.List;
+
 import org.springframework.data.repository.PagingAndSortingRepository
 import org.springframework.data.repository.query.Param
 import org.springframework.data.rest.core.annotation.RepositoryRestResource
